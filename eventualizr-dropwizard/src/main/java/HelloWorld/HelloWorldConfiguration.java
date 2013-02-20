beada69a976ae176c5664dc84e583f7861e6094f
Jeroen <jeroen.boone.cegeka@outlook.com>
2013-02-20 14:29:07 +0100

    jeroen simple hello world

---
+++ eventualizr-dropwizard/src/main/java/HelloWorld/HelloWorldConfiguration.java
@@ -0,0 +1,8 @@
+package HelloWorld;
+
+import com.yammer.dropwizard.config.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class HelloWorldConfiguration extends Configuration {
+}
