26fc619f880c48ce79911bfb376944f7377d1bc2
Wim Rutgeerts <wim.rutgeerts@gmail.com>
2013-02-20 16:52:49 +0100

    robin wim: added bootstrap css, ui that shows the meetings and async gets the talks

---
+++ eventualizr-web-angular-robin-wim/src/main/webapp/js/MeetingController.js
@@ -0,0 +1,21 @@
+function MeetingController($scope, $http){
+
+	$http.get('api/meetings').success(function(data, status, headers, config){
+		$scope.meetings = data;
+	})
+	.error(function(){
+		alert("backend not responding");
+	}
+	);
+
+	$scope.getTalks = function ($meeting){
+		$http.get('api/meetings/'+$meeting.id+'/talks').success(function(data, status, headers, config){
+			$meeting.talks = data;
+		})
+		.error(function(){
+			alert("backend not responding");
+		}
+		);
+	}
+
+}
