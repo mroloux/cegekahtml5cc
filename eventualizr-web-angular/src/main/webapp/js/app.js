01a34e3aedb06aeabd49e9b18b7aa70fdb2358e6
Kaat B <kaat.bogaerts@cegeka.be>
2013-04-17 16:45:11 +0200

    kaat: layout + datepickers

--- eventualizr-web-angular/src/main/webapp/js/app.js
+++ eventualizr-web-angular/src/main/webapp/js/app.js
@@ -4,7 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('eventualizrApp', ['ngResource', 'eventualizrApp.filters', 'eventualizrApp.services', 'eventualizrApp.directives', '$strap.directives']).
   config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: MeetingListController});
 	$routeProvider.when('/meetings', {templateUrl: 'partials/meeting-list.html', controller: MeetingListController});
 	$routeProvider.when('/meetings/:meetingId', {templateUrl: 'partials/meeting-detail.html', controller: MeetingDetailController});
-    $routeProvider.otherwise({redirectTo: '/meetings'});
+    $routeProvider.otherwise({redirectTo: '/home'});
   }]);
--- eventualizr-web-angular/src/main/webapp/js/controllers.js
+++ eventualizr-web-angular/src/main/webapp/js/controllers.js
@@ -5,13 +5,21 @@
 function MeetingListController($scope, Meeting) {
 	$scope.resetNewMeeting = function() {
     	$scope.newMeeting = new Meeting();
+    	$scope.startdate = new Object();
+    	$scope.startdate.date = null;
+    	$scope.startdate.time = "00:00";
+    	$scope.enddate = new Object();
+    	$scope.enddate.date = null;
+    	$scope.enddate.time = "00:00";
     };
     
 	$scope.meetings = Meeting.query();
     $scope.orderProp = 'datum';
     $scope.resetNewMeeting();
     
-    $scope.update = function(newMeeting) {
+    $scope.update = function(newMeeting, startdate, enddate) {
+    	newMeeting.start = createDateFromDateTimePickers(startdate);
+    	newMeeting.end = createDateFromDateTimePickers(enddate);
     	newMeeting.$save({}, function(data) {
     		$scope.meetings = Meeting.query();
     		$scope.resetNewMeeting();
@@ -19,17 +27,24 @@ function MeetingListController($scope, Meeting) {
             console.log(data);               
         });
     };
-    $scope.remove = function(meeting) {
-    	meeting.$delete({meetingId:meeting.id}, function() {
-    		$scope.meetings = Meeting.query();
-    	});
-    };
+    function createDateFromDateTimePickers(datum){
+    	datum.date.setHours(datum.time.split(':')[0]);
+    	datum.date.setMinutes(datum.time.split(':')[1]);
+    	return datum.date.toJSON().replace('Z','');
+    }
 }
 MeetingListController.$inject = ['$scope', 'Meeting'];
 
 
-function MeetingDetailController($scope, $routeParams, Meeting, Talk) {
+function MeetingDetailController($scope, $routeParams, $location, Meeting, Talk) {
     $scope.talks = Talk.query({meetingId: $routeParams.meetingId});
     $scope.meeting = Meeting.get({meetingId: $routeParams.meetingId});
+   
+    $scope.remove = function(meeting) {
+    	meeting.$delete({meetingId:meeting.id}, function() {
+    		$scope.meetings = Meeting.query();
+    		$location.path('/meetings');
+    	});
+    };
 }
-MeetingDetailController.$inject = ['$scope', '$routeParams', 'Meeting', 'Talk'];
+MeetingDetailController.$inject = ['$scope', '$routeParams', '$location', 'Meeting', 'Talk'];
