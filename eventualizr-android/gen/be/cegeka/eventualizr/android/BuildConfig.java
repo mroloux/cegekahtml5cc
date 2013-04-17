010e78af4ae79ad910ddbccfa5eb4269382957e8
Mercer Traieste <mercer.traieste@gmail.com>
2013-04-17 12:38:06 +0300

    Added Eventualizr android module, partial maven suport

---
+++ eventualizr-android/gen/be/cegeka/eventualizr/android/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package be.cegeka.eventualizr.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
