44244a336ea7b2ebd483f1d99ba2131ec9d1a68a
Kaat B <kaat.bogaerts@cegeka.be>
2013-05-15 10:29:37 +0200

    kaat: input components in directives

--- eventualizr-web-angular/src/main/webapp/js/directives.js
+++ eventualizr-web-angular/src/main/webapp/js/directives.js
@@ -2,10 +2,31 @@
 
 /* Directives */
 
-
-angular.module('eventualizrApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+angular.module('eventualizrApp.directives', [])
+	.directive('appVersion',
+		[ 'version', function(version) {
+			return function(scope, elm, attrs) {
+				elm.text(version);
+			};
+		} ])
+	
+	.directive('inputcomponent', function factory() {
+		return {
+			templateUrl : 'partials/components/inputcomponent.html',
+			restrict : 'A',
+			scope : {
+				label:"@",
+				guimodel:"="
+			}
+		};
+	})
+	.directive('datetimecomponent', function factory() {
+		return {
+			templateUrl : 'partials/components/datetimecomponent.html',
+			restrict : 'A',
+			scope : {
+				label:"@",
+				guimodel:"="
+			}
+		};
+	});
